09df59a9088f742546e8cf59a409c744f8e139c9
Krit <tanakritkhunkhae@gmail.com>
2023-12-02 13:06:51 +0700

    on scroll add class-list

--- src/components/cursor.tsx
+++ src/components/cursor.tsx
@@ -1,20 +1,23 @@
-"use client"
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import '../style/cursor.css'
+import React, { useState, useEffect } from "react";
+import "../style/cursor.css";
 
 function Cursor() {
-    const [mousePosition, setMousePosition] = useState({left:-30,top:-30})
+  const [mousePosition, setMousePosition] = useState({ left: -30, top: -30 });
 
-		useEffect(() => {
-			document. addEventListener('mousemove', (e) => handleMouseMove(e))
-		},[])
-    const handleMouseMove = (e: globalThis.MouseEvent) => {
-        setMousePosition({left:e.clientX,top:e.clientY})
-    }
-    return ( 
-        <div className="inner-cursor" style={{left:mousePosition.left, top:mousePosition.top}}/>
-    );
+  useEffect(() => {
+    document.addEventListener("mousemove", (e) => handleMouseMove(e));
+  }, []);
+  const handleMouseMove = (e: globalThis.MouseEvent) => {
+    setMousePosition({ left: e.clientX, top: e.clientY });
+  };
+  return (
+    <div
+      className="inner-cursor"
+      style={{ left: mousePosition.left, top: mousePosition.top }}
+    />
+  );
 }
 
 export default Cursor;
